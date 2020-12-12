4ee13508e6b7def23ce2dae732604a6a3d819646
Ana Rute Mendes <anarutecvm@gmail.com>
2020-12-12 10:31:37 -0300

    change thumbs animation on mouseover

--- components/Sonhos.tsx
+++ components/Sonhos.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import VideoPlayer from "./VideoPlayer";
 import styles from "../styles/Home.module.css";
 
@@ -6,17 +6,12 @@ export default function Sonhos(props) {
   const { sonhosData } = props;
   const [showVideo, setShowVideo] = useState(false);
   const [currentVideo, setCurrentVideo] = useState("");
-  const [currentThumb, setCurrentThumb] = useState("");
 
   const toggleShowVideo = (video: string) => {
     console.log(video);
     setCurrentVideo(video);
 
     setShowVideo(!showVideo);
-  };
-
-  const toggleShowThumb = (id: string) => {
-    setCurrentThumb(id);
   };
 
   const SonhosMenu = () => (
@@ -27,46 +22,32 @@ export default function Sonhos(props) {
           onClick={() => {
             toggleShowVideo(sonho.video);
           }}
-          onMouseEnter={() => {
-            toggleShowThumb(sonho.id);
-          }}
-          onMouseOut={() => {
-            toggleShowThumb("");
-          }}
-          className={styles.test}
+          className={styles.submenuLink}
           id={`link-${sonho.id}`}
         >
           {sonho.name}
+          <div
+            key={`thumb-${sonho.id}`}
+            className={
+              sonho.equipe
+                ? `${styles.sonhoThumb} ${styles.thumbEquipe}`
+                : `${styles.sonhoThumb} ${styles.thumbEntrevista}`
+            }
+            onClick={() => {
+              toggleShowVideo(sonho.video);
+            }}
+            id={`thumb-${sonho.id}`}
+          >
+            <img src={sonho.thumb} />
+          </div>
         </li>
       ))}
     </ul>
   );
 
-  const SonhosElements = () => (
-    <div className={styles.sonhosContainer}>
-      {sonhosData.map((sonho) => (
-        <div
-          key={`thumb-${sonho.id}`}
-          className={
-            currentThumb == sonho.id
-              ? `${styles.sonhoThumb} ${styles.fullOpacity}`
-              : styles.sonhoThumb
-          }
-          onClick={() => {
-            toggleShowVideo(sonho.video);
-          }}
-          id={`thumb-${sonho.id}`}
-        >
-          <img src={sonho.thumb} />
-        </div>
-      ))}
-    </div>
-  );
-
   return (
     <div className={styles.thumbWrapper}>
       <SonhosMenu />
-      <SonhosElements />
       <div
         className={
           showVideo
